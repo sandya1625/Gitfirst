70ede2092b7dedec3f91b9c5ebaab134b32daffe
sandya <sandyamakkina@gmail.com>
2022-07-30 16:23:33 +0530

    First GIT Commit

---
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	@Test
+	public void PostJira()
+	{
+		System.out.println("PostJira");
+	}
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
